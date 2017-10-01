cdc58e5cd603ed628f21f0dacb0a55e6c06e9a8a
LuckThemAll <LuckThemAll@gmail.com>
2017-10-01 20:58:31 +1000

    bats add

---
+++ bats/create_DB.bat
@@ -0,0 +1,7 @@
+SET isql=C:\"Program Files"\Firebird\Firebird_2_5\bin\isql.exe -u SYSDBA -p masterkey
+SET sqlfolder=C:\Users\Artem\Desktop\DB\Sql
+
+%isql% -i %sqlfolder%\students.sql
+%isql% -i %sqlfolder%\groups.sql
+%isql% -i %sqlfolder%\subjects.sql
+%isql% -i %sqlfolder%\marks.sql
