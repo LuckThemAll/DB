ae4ed7bb778ff2f079ac7455980819e0c29b5e14
LuckThemAll <LuckThemAll@gmail.com>
2017-11-23 09:05:18 +1000

    spaces add

--- ScheduleDB/app.py
+++ ScheduleDB/app.py
@@ -24,26 +24,31 @@ tables_dict = {
 def index():
     data = {}
     data['tables'] = [tables_dict[table].title for table in tables_dict]
+
     selected_table_index = request.args.get('tables', '')
     search_str = request.args.get('sub_search', '')
     selected_col_name_index = request.args.get('search_col', '')
+
     if selected_col_name_index == '':
         selected_col_name_index = 0
     if (selected_table_index.isdigit() and int(selected_table_index) >= 0 and int(selected_table_index) < len(tables_dict)):
         selected_table = int(selected_table_index)
+
         selected_col_name_index = int(selected_col_name_index)
         data['search_str'] = search_str
         data['selected_table_index'] = selected_table
+
         tables_list = [item for item in tables_dict]
         selected_table = tables_dict[tables_list[selected_table]]
         cols_list = [item for item in selected_table.__dict__['columns']]
+
         if selected_col_name_index >= cols_list.__len__():
             selected_col_name_index = 0
-        data['selected_col_name_index'] = selected_col_name_index
         selected_col_name = selected_table.columns[cols_list[selected_col_name_index]].get_title()
+
+        data['selected_col_name_index'] = selected_col_name_index
         data['selected_col_name'] = selected_col_name
         data['search_tables'] = [selected_table.columns[item].get_title() for item in selected_table.__dict__['columns']]
         data['headers'] = selected_table.get_titles()
-        selected_table.get_rows((cols_list[selected_col_name_index], search_str))
         data['records'] = selected_table.get_rows((cols_list[selected_col_name_index], search_str))
     return render_template('back_ground.html', **data)
