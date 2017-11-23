fe43b4456123959d67cbb5154c207331aaba7bba
LuckThemAll <LuckThemAll@gmail.com>
2017-11-23 23:25:31 +1000

    sqlBuilder updated

--- ScheduleDB/sqlBuilred.py
+++ ScheduleDB/sqlBuilred.py
@@ -2,45 +2,30 @@ from fields import *
 
 
 class SQLBuilder:
+    query = 'select '
+    fields = []
+    l_join = []
+    where_param = []
+    from_table = 'form '
+
     def __init__(self, table):
-        self.query = 'select '
         self.table = table
-    pass
+        self.table_name = self.table.table_name
 
     def add_fields(self):
-        for i, col in enumerate(self.table.get_col_names()):
-            if i == 0:
-                self.query += col
-            else:
-                self.query += ', ' + col
-        self.query += ' '
+        for col in self.table.get_col_names():
+            self.fields.append(col)
 
     def add_from_table(self):
-        self.query += 'from ' + self.table.table_name + ' '
+        self.from_table += self.table_name + ' '
 
     def add_l_joins(self):
-        cols = self.table.columns
-        refs_fields = (item for item in cols if type(cols[item]) is ReferenceField)
-        for item in refs_fields:
-            col = cols[item]
-            self.query += ('left join {0} on {0}.{1}={2}.{3} '.format(col.source.table_name,
-                                                                      col.ref,
-                                                                      self.table.table_name,
-                                                                      item))
+        for key, val in self.table.columns.items():
+            if isinstance(val, ReferenceField):
+                self.l_join.append((val.source.table_name, val.ref, self.table_name, key))
 
     def add_where(self, wheres):
-        self.query += 'where ' + self.table.get_tab_col(wheres[0]) + ' containing ? '
-
-    def create_sql(self, wheres=()):
-        self.add_fields()
-        self.add_from_table()
-        print([self.table.columns[item] for item in self.table.columns])
-        for item in self.table.columns:
-            if isinstance(self.table.columns[item], ReferenceField):
-                self.add_l_joins()
-                break
-        if wheres[1]:
-            self.add_where(wheres)
+        self.where_param.append(self.table.get_tab_col(wheres[0]))
 
     def get_sql(self):
         return self.query
